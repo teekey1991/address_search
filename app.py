8c568e79542cdd229f2de33bd8562df15fcc5cb6
teekey1991 <matsunaetaiki@gmail.com>
2022-06-25 15:55:49 +0900

    住所表示をできるようにした

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from urllib import response
 import requests
 
 
-#put = input("郵便番号を入力ください > ")
-postal_code = ("0028061")
+# postal_code = input("郵便番号を入力ください > ")
+postal_code = "0287111"
 
-response = requests.get(f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={postal_code}")
+response = requests.get(
+    f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={postal_code}"
+)
 
 print(response)
-#print(response.text)
-dic = response.json()
-print(dic)
+# print(response.text)  #テキスト
+dic = response.json() #パース
+#print(dic)
+pref_name = dic['results'][0]['address1'] #都道府県
+city_name = dic['results'][0]['address2'] #市区
+town_name = dic['results'][0]['address3'] #町村
+
+print(f"{pref_name}{city_name}{town_name}")
